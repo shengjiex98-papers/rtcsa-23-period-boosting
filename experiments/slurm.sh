2c2dfd5abf013b1a679bf87cd49c774d7bfbf344
Shengjie Xu <shengjiex98@gmail.com>
2022-08-21 00:07:00 -0400

    Changed scripts

--- experiments/slurm.sh
+++ experiments/slurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=48:00:00
 
 module add julia
-julia --project=.. --threads 8 -e 'import Pkg; Pkg.instantiate()'
-julia --project=.. --threads 8 experiment.jl "AP" "HK"
+julia --project=.. --threads 8 -e 'import Pkg; Pkg.instantiate(); include("job_dispatch.jl")'
+### julia --project=.. --threads 8 experiment.jl "AP" "HK"
 
 ### sbatch -p general -N 1 --mem 4096 -n 1 -c 16 -t 48:00:00 --mail-type=end --mail-user=sxunique@cs.unc.edu --wrap='julia --threads 16 experiment.jl "ES" "HK"'
