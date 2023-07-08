c809793f1f788e533203c2368c6b60ec3834895e
shengjiex98 <shengjiex98@gmail.com>
2023-07-08 00:32:11 -0400

    rtcsa_camera_ready_freeze

--- lib/Experiments.jl
+++ lib/Experiments.jl
@@ -1,16 +1,48 @@
 module Experiments
 
 using ControlSystemsBase
+using Plots, LaTeXStrings
 
 using ControlSafetyBench
 using ControlTimingSafety
 using RealTimeScheduling
 
-export nominal_trajectory_1, x_to_z_kill, x_to_z_skip_next, nominal_trajectory_2, slice_nominal, period_boosting
+export nominal_trajectory_1, x_to_z_kill, x_to_z_skip_next, nominal_trajectory_2, slice_nominal, period_boosting, plotdev
 
-function period_boosting(sysc::AbstractStateSpace{<:Continuous}, K::AbstractMatrix{<:Real},
-        newp::Integer, z0::AbstractVecOrMat{<:Real}, d_max::Real, maxwindow::Integer, 
-        n::Integer, nominal::Matrix{<:Real})
+function plotdev(sysc::AbstractStateSpace{<:Continuous}, 
+        K::AbstractMatrix{<:Real}, p::Integer, newp::Integer, 
+        z0::AbstractVecOrMat{<:Real}, safety_margin::Real, H::Integer, 
+        az::Real, el::Real, ar::Real=1)
+    # TODO: make nominal include full state, not just output (so we have 3-d plots)
+    sysd = c2d(sysc, 0.001*p)
+    
+    a = hold_kill(sysd, K, MeetAny(1, 1))
+    input = ones(Int64, H)
+    nominal = evol(a, z0, input)'
+
+    # Create empty plot with three dimensions
+	plt = plot(xlabel=L"x_1", ylabel=L"x_2", zlabel=L"t",
+               camera=(az,el), aspect_ratio=ar)
+
+    # Plot nominal trajectory
+    plot!(nominal[1,:], nominal[2,:], 1:H+1, label="Nominal",
+          color=:black, linewidth=1)
+
+    # Plot safety pipe
+	θ = LinRange(0, 2π, 40)
+	circ_x = cos.(θ) * safety_margin
+	circ_y = sin.(θ) * safety_margin
+	for i in 1:H+1
+		plot!(circ_x .+ nominal[1,i], circ_y .+ nominal[2,i], 
+        repeat([i], size(θ,1)), label=(i == 1) ? "Safety Margin" : "", 
+        seriestype=[:shape,], color=:lightblue)
+	end
+
+    plt
+end
+
+function period_boosting(sysc::AbstractStateSpace{<:Continuous}, 
+        K::AbstractMatrix{<:Real}, newp::Integer, z0::AbstractVecOrMat{<:Real}, d_max::Real, maxwindow::Integer, n::Integer, nominal::Matrix{<:Real})
     sysd = c2d(sysc, 0.001*newp)
     nominal = slice_nominal(nominal, newp)
     H = size(nominal, 2) - 1
@@ -20,9 +52,8 @@ function period_boosting(sysc::AbstractStateSpace{<:Continuous}, K::AbstractMatr
 end
 
 function nominal_trajectory_1(sysc::AbstractStateSpace{<:Continuous},
-        x0::Real, p_ms::Integer, H::Integer)
-    sysd = c2d(sysc, 0.001*p_ms)
-    K = delay_lqr(sysc, 0.001*p_ms)
+        K::AbstractMatrix{<:Real}, x0::Real, p::Integer, H::Integer)
+    sysd = c2d(sysc, 0.001*p)
     
     a = hold_kill(sysd, K, MeetAny(1, 1))
     z0 = x_to_z_kill(sysd, x0)
@@ -32,17 +63,16 @@ function nominal_trajectory_1(sysc::AbstractStateSpace{<:Continuous},
 end
 
 function nominal_trajectory_2(sysc::AbstractStateSpace{<:Continuous},
-        x0::Real, p_ms::Integer, H::Integer)
+        K::AbstractMatrix{<:Real}, x0::Real, p::Integer, H::Integer)
     # Default with 1ms intervals
     sysd = c2d(sysc, 0.001)
-    K = delay_lqr(sysc, 0.001*p_ms)
     
     # No deadline requirements as we will be supplying the hit/miss pattern
     a = hold_skip_next(sysd, K)
     z0 = x_to_z_skip_next(sysd, x0)
 
-    # Only the p_ms-th deadline is hit
-    input = Vector{Int64}((1:(H*p_ms)) .% p_ms .== 0)
+    # Only the p-th deadline is hit
+    input = Vector{Int64}((1:(H*p)) .% p .== 0)
     # Convert to 1=hit, 2=miss
     input = 2 .- input
 
@@ -57,8 +87,8 @@ function x_to_z_skip_next(sys::AbstractStateSpace, x0::Real, u0::Real=0)
     vcat(repeat([Float64(x0)], sys.nx), repeat([0.0], sys.nx), repeat([Float64(u0)], sys.nu))
 end
 
-function slice_nominal(nom::AbstractMatrix{<:Real}, p_ms::Integer)
-    nom[:, (1:end) .% p_ms .== 1]
+function slice_nominal(nom::AbstractMatrix{<:Real}, p::Integer)
+    nom[:, (1:end) .% p .== 1]
 end
     
 end
---
+++ src/analysis.jl
@@ -0,0 +1,243 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ ba91b96e-1c2a-11ee-35f8-2183fe15f018
+begin
+	import Pkg
+	Pkg.activate("..")
+
+	using RealTimeScheduling
+	using ControlTimingSafety
+	using ControlSafetyBench
+	
+	using Plots, LaTeXStrings, PlutoUI
+	using DelimitedFiles
+
+	push!(LOAD_PATH, "../lib")
+	# include("../lib/Experiments.jl")
+	using Experiments
+	
+	TableOfContents()
+end
+
+# ╔═╡ 6e8ceb64-c5a7-4dee-8078-ce5aed37a169
+md"""
+# Schedule Synthesis Analysis
+"""
+
+# ╔═╡ 5470e671-b29a-44e1-9bdd-e2cb20937c52
+md"""
+## Reading Data from Files
+"""
+
+# ╔═╡ 4eb354c4-459c-402b-9bcd-2c4bd00518bd
+systems = [:RC, :F1, :DC, :CS, :CC]
+
+# ╔═╡ 97f1fd06-cebf-427d-a107-75edf27cc544
+ps = [15, 28, 40]
+
+# ╔═╡ 02ab2b94-f667-483c-9d20-4e12f836436d
+rs = ["", "-RE"]
+
+# ╔═╡ c0fb0533-9826-4d78-8ae2-4ef811fa3630
+data = let
+	dir = "../data/"
+	rdev(sys, p, r) = readdlm("$dir$sys-$p$r.tsv", '\t', Float64, '\n')
+	list = map(Iterators.product(systems, ps, rs)) do (sys, p, r)
+		Symbol(sys, p, r) => rdev(sys, p, r)
+	end
+	(; list...)
+end
+
+# ╔═╡ f25c2946-3a77-4c28-b720-afbe11047721
+md"""
+## Finding safe constraints
+"""
+
+# ╔═╡ e96a3c24-e916-4b60-b380-59ea113f9810
+saf = Dict()
+
+# ╔═╡ f4183950-4f18-420b-b592-7669e3343194
+@bind tRC Slider(1:0.01:10, default=5.8, show_value=true)
+
+# ╔═╡ b65da002-1de4-485e-93a7-06722af981c3
+@bind tF1 Slider(0.1:0.001:1, default=0.37, show_value=true)
+
+# ╔═╡ fce45e39-6f86-4ef5-a0d2-15b9c2bdd434
+@bind tDC Slider(0.001:0.001:0.5, default=0.18, show_value=true)
+
+# ╔═╡ 197931fb-d60a-4434-8d77-3f079793ff88
+@bind tCS Slider(0.1:0.005:5, default=2.1, show_value=true)
+
+# ╔═╡ 37087901-56b9-4e2b-beef-3ebf5d0c85a8
+@bind tCC Slider(0.05:0.005:1, default=0.48, show_value=true)
+
+# ╔═╡ e32ad5df-0692-4870-8ff8-e3a4c106060d
+function find_safe!(saf, data, sys::Symbol, t::Real)
+	maxwindow = size(data[Symbol(sys, 15)], 1)
+	res = fill("", maxwindow, maxwindow)
+	for p in ps, r in rs
+		id = Symbol(sys, p, r)
+		saf[id] = Vector{MeetAny{Int64}}()
+		for window = 1:maxwindow, meet = 1:window
+			if data[id][window, meet] < t 
+				res[window, meet] *= "$(p÷10)"
+				push!(saf[id], MeetAny(meet, window))
+			else
+				res[window, meet] *= " "
+			end
+		end
+	end
+	@info res
+	saf
+end
+
+# ╔═╡ c3d7f90c-6962-4eca-bbc0-20d0af4125aa
+r1 = find_safe!(saf, data, :RC, tRC);
+
+# ╔═╡ 045260ed-6a24-4359-ae7c-ff12dcc8a20c
+r2 = find_safe!(saf, data, :F1, tF1);
+
+# ╔═╡ e2493314-841f-42d1-9a3d-7e334650e9ec
+r3 = find_safe!(saf, data, :DC, tDC);
+
+# ╔═╡ 7f5b7f07-e5fe-4425-a59d-3b37bcc53778
+r4 = find_safe!(saf, data, :CS, tCS);
+
+# ╔═╡ a402a64a-2a42-42a2-9d49-b4a225119865
+r5 = find_safe!(saf, data, :CC, tCC);
+
+# ╔═╡ 0154f2df-6e1a-4d66-b3a9-08b96d682608
+md"""
+Now, use the safe constraints to attempt schedule synthesis
+"""
+
+# ╔═╡ 28d67e5c-378a-4863-8cab-731cbea17f63
+s15 = let
+	r1, r2, r3, r4, r5
+	consn = map(systems) do sys
+		saf[Symbol(sys, 15)]
+	end
+	consr = map(systems) do sys
+		saf[Symbol(sys, 15, "-RE")]
+	end
+	schedule_xghtc(consn, 100, slotsize=1),
+	schedule_xghtc(consr, 100, slotsize=1)
+end
+
+# ╔═╡ 9c76bed6-0e5a-4219-8881-b22d418b05a2
+s28 = let
+	r1, r2, r3, r4, r5
+	consn = map(systems) do sys
+		saf[Symbol(sys, 28)]
+	end
+	consr = map(systems) do sys
+		saf[Symbol(sys, 28, "-RE")]
+	end
+	schedule_xghtc(consn, 100, slotsize=2, fullpath=true),
+	schedule_xghtc(consr, 100, slotsize=2, fullpath=true)
+end
+
+# ╔═╡ 37da5df9-103b-413d-9de0-9693f1eb7263
+s40 = let
+	r1, r2, r3, r4, r5
+	consn = map(systems) do sys
+		saf[Symbol(sys, 40)]
+	end
+	consr = map(systems) do sys
+		saf[Symbol(sys, 40, "-RE")]
+	end
+	schedule_xghtc(consn, 100, slotsize=3, fullpath=true),
+	schedule_xghtc(consr, 100, slotsize=3, fullpath=true)
+end
+
+# ╔═╡ 516e761e-d193-45a5-9931-850d99c6f5fa
+md"""
+## Deviation Visualization
+"""
+
+# ╔═╡ 9ef447f4-d849-4807-8546-411f55021821
+begin
+	H = 100
+	d_max = 1000
+	maxwindow = 6
+	params = (
+	    RC = (p = 23, d = 5.8, x0 = 100, n = 10),
+	    F1 = (p = 20, d = 0.37, x0 = 1,   n = 15),
+	    DC = (p = 23, d = 0.18, x0 = 100, n = 10),
+	    CS = (p = 27, d = 2.0, x0 = 100, n = 15),
+	    CC = (p = 28, d = 4.5, x0 = 10,  n = 15))
+	K = (
+	    RC = delay_lqr(benchmarks[:RC], 0.001*params[:RC][:p]),
+	    F1 = delay_lqr(benchmarks[:F1], 0.001*params[:F1][:p]),
+	    DC = delay_lqr(benchmarks[:DC], 0.001*params[:DC][:p]),
+	    CS = delay_lqr(benchmarks[:CS], 0.001*params[:CS][:p]),
+	    CC = delay_lqr(benchmarks[:CC], 0.001*params[:CC][:p]))
+	K[:RC]
+end
+
+# ╔═╡ 4858b2e4-c2dd-4ab1-bfbb-862d16802bf5
+md"""
+### Camera Settings
+
+|   |   |
+|--:|:--|
+| Azimuth 	| $(@bind az Slider(-180:1:180, default=35, show_value=true)) |
+| Elevation | $(@bind el Slider(-90:1:90,   default=25, show_value=true)) |
+"""
+
+# ╔═╡ d07fe9f5-c1da-48ef-a58d-d865be62eb33
+# ╠═╡ disabled = true
+#=╠═╡
+pd(s::Symbol, newp::Integer) = plotdev(benchmarks[s], K[s], newp,
+	x_to_z_kill(benchmarks[s], params[s][:x0]), params[s][:d], H, az, el)
+  ╠═╡ =#
+
+# ╔═╡ 22dbd804-71af-4319-955c-4684364225bc
+#=╠═╡
+pd(:F1, 28)
+  ╠═╡ =#
+
+# ╔═╡ Cell order:
+# ╟─6e8ceb64-c5a7-4dee-8078-ce5aed37a169
+# ╠═ba91b96e-1c2a-11ee-35f8-2183fe15f018
+# ╟─5470e671-b29a-44e1-9bdd-e2cb20937c52
+# ╠═4eb354c4-459c-402b-9bcd-2c4bd00518bd
+# ╠═97f1fd06-cebf-427d-a107-75edf27cc544
+# ╠═02ab2b94-f667-483c-9d20-4e12f836436d
+# ╠═c0fb0533-9826-4d78-8ae2-4ef811fa3630
+# ╟─f25c2946-3a77-4c28-b720-afbe11047721
+# ╠═e96a3c24-e916-4b60-b380-59ea113f9810
+# ╠═f4183950-4f18-420b-b592-7669e3343194
+# ╠═c3d7f90c-6962-4eca-bbc0-20d0af4125aa
+# ╠═b65da002-1de4-485e-93a7-06722af981c3
+# ╠═045260ed-6a24-4359-ae7c-ff12dcc8a20c
+# ╠═fce45e39-6f86-4ef5-a0d2-15b9c2bdd434
+# ╠═e2493314-841f-42d1-9a3d-7e334650e9ec
+# ╠═197931fb-d60a-4434-8d77-3f079793ff88
+# ╠═7f5b7f07-e5fe-4425-a59d-3b37bcc53778
+# ╠═37087901-56b9-4e2b-beef-3ebf5d0c85a8
+# ╠═a402a64a-2a42-42a2-9d49-b4a225119865
+# ╠═e32ad5df-0692-4870-8ff8-e3a4c106060d
+# ╟─0154f2df-6e1a-4d66-b3a9-08b96d682608
+# ╠═28d67e5c-378a-4863-8cab-731cbea17f63
+# ╠═9c76bed6-0e5a-4219-8881-b22d418b05a2
+# ╠═37da5df9-103b-413d-9de0-9693f1eb7263
+# ╟─516e761e-d193-45a5-9931-850d99c6f5fa
+# ╟─9ef447f4-d849-4807-8546-411f55021821
+# ╟─4858b2e4-c2dd-4ab1-bfbb-862d16802bf5
+# ╠═d07fe9f5-c1da-48ef-a58d-d865be62eb33
+# ╠═22dbd804-71af-4319-955c-4684364225bc
